5a430ce4590be4d283dc58c6ac4470daa621b186
ManuchehrM <57656329+ManuchehrM@users.noreply.github.com>
2020-06-02 23:40:54 +0500

    dump file updated
    dump file updated

--- database.sql
+++ database.sql
@@ -70,18 +70,12 @@ CREATE TABLE `notifications_log` (
   PRIMARY KEY (`id`),
   KEY `notifications_log_user_id_foreign` (`user_id`),
   CONSTRAINT `notifications_log_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `notifications_log` */
 
 insert  into `notifications_log`(`id`,`orders`,`user_id`,`message`,`status_description`,`status`,`notify_type`,`created_at`,`updated_at`) values 
-(1,'[{\"name\":\"Samsung S6 Edge\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"Samsung S5\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"IPhone X\",\"phone\":',2,'Thank you for your purchase!!!','success',1,'email','2020-06-02 13:32:54','2020-06-02 13:32:54'),
-(2,'[{\"name\":\"Samsung S6 Edge\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"Samsung S5\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"IPhone X\",\"phone\":',2,'Thank you for your purchase!!!','success',1,'email','2020-06-02 13:37:41','2020-06-02 13:37:41'),
-(3,'[{\"name\":\"Samsung S6 Edge\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"Samsung S5\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"IPhone X\",\"phone\":',2,'Thank you for your purchase!!!','554 - Expected response code 250 but got code \"554\", with message \"554 5.7.1 [2] Message rejected under suspicion of SPAM; https://ya.cc/1IrBc 1591105070-ioOx63ynHU-bo2C7bnQ\r\n\"',0,'email','2020-06-02 13:37:50','2020-06-02 13:37:50'),
-(4,'[{\"name\":\"Samsung S6 Edge\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"Samsung S5\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"IPhone X\",\"phone\":',2,'Thank you for your purchase!!!','success',1,'email','2020-06-02 13:39:30','2020-06-02 13:39:30'),
-(5,'[{\"name\":\"Samsung S6 Edge\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"Samsung S5\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"IPhone X\",\"phone\":',2,'Thank you for your purchase!!!','success',1,'email','2020-06-02 14:31:15','2020-06-02 14:31:15'),
-(6,'[{\"name\":\"Samsung S6 Edge\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"Samsung S5\",\"phone\":\"992927001911\",\"email\":\"muzafarov9797@gmail.com\"},{\"name\":\"IPhone X\",\"phone\":',2,'Thank you for your purchase!!!','success',1,'phone','2020-06-02 14:32:06','2020-06-02 14:32:06'),
-(7,'[{\"name\":\"IPhone X\",\"phone\":\"992927001911\",\"email\":\"user1@gmail.com\"},{\"name\":\"IPhone X\",\"phone\":\"992927001911\",\"email\":\"user1@gmail.com\"},{\"name\":\"Samsung S5\",\"phone\":\"992927001911\",\"email\":',3,'Thank you for your purchase!!!','554 - Expected response code 250 but got code \"554\", with message \"554 5.7.1 [2] Message rejected under suspicion of SPAM; https://ya.cc/1IrBc 1591108974-m4wXB3QkuZ-gskWV1KL\r\n\"',0,'email','2020-06-02 14:42:54','2020-06-02 14:42:54');
+(1,'[{\"name\":\"Samsung S5\",\"phone\":\"992917004471\",\"email\":\"manu6699@mail.ru\"},{\"name\":\"Samsung S10\",\"phone\":\"992917004471\",\"email\":\"manu6699@mail.ru\"},{\"name\":\"IPhone X\",\"phone\":\"992917004471\",\"em',2,'Thank you for your purchase!!!','success',1,'email','2020-06-02 18:35:27','2020-06-02 18:35:27');
 
 /*Table structure for table `order` */
 
@@ -105,26 +99,26 @@ CREATE TABLE `order` (
 /*Data for the table `order` */
 
 insert  into `order`(`id`,`user_id`,`product_id`,`status`,`quantity`,`created_at`,`updated_at`) values 
-(1,2,2,0,3,'2020-06-02 07:30:56',NULL),
-(2,2,1,0,1,'2020-06-02 07:30:56',NULL),
-(3,2,5,0,4,'2020-06-02 07:30:56',NULL),
-(4,2,3,0,3,'2020-06-02 07:30:56',NULL),
-(5,2,5,0,5,'2020-06-02 07:30:56',NULL),
-(6,3,5,0,4,'2020-06-02 07:30:56',NULL),
-(7,3,5,0,1,'2020-06-02 07:30:56',NULL),
-(8,3,1,0,1,'2020-06-02 07:30:56',NULL),
-(9,3,1,0,5,'2020-06-02 07:30:56',NULL),
-(10,3,2,0,1,'2020-06-02 07:30:56',NULL),
-(11,4,3,0,1,'2020-06-02 07:30:56',NULL),
-(12,4,1,0,5,'2020-06-02 07:30:56',NULL),
-(13,4,4,0,5,'2020-06-02 07:30:56',NULL),
-(14,4,4,0,2,'2020-06-02 07:30:56',NULL),
-(15,4,2,0,2,'2020-06-02 07:30:56',NULL),
-(16,5,4,0,3,'2020-06-02 07:30:56',NULL),
-(17,5,5,0,5,'2020-06-02 07:30:56',NULL),
-(18,5,5,0,2,'2020-06-02 07:30:56',NULL),
-(19,5,4,0,1,'2020-06-02 07:30:56',NULL),
-(20,5,2,0,2,'2020-06-02 07:30:56',NULL);
+(1,2,1,0,2,'2020-06-02 18:35:00',NULL),
+(2,2,4,0,4,'2020-06-02 18:35:00',NULL),
+(3,2,5,0,1,'2020-06-02 18:35:00',NULL),
+(4,2,2,0,5,'2020-06-02 18:35:00',NULL),
+(5,2,4,0,5,'2020-06-02 18:35:00',NULL),
+(6,3,3,0,4,'2020-06-02 18:35:00',NULL),
+(7,3,2,0,1,'2020-06-02 18:35:00',NULL),
+(8,3,5,0,2,'2020-06-02 18:35:00',NULL),
+(9,3,3,0,4,'2020-06-02 18:35:00',NULL),
+(10,3,5,0,4,'2020-06-02 18:35:00',NULL),
+(11,4,1,0,5,'2020-06-02 18:35:00',NULL),
+(12,4,5,0,2,'2020-06-02 18:35:00',NULL),
+(13,4,1,0,5,'2020-06-02 18:35:00',NULL),
+(14,4,5,0,5,'2020-06-02 18:35:00',NULL),
+(15,4,3,0,3,'2020-06-02 18:35:00',NULL),
+(16,5,2,0,4,'2020-06-02 18:35:00',NULL),
+(17,5,2,0,1,'2020-06-02 18:35:00',NULL),
+(18,5,3,0,4,'2020-06-02 18:35:00',NULL),
+(19,5,1,0,2,'2020-06-02 18:35:00',NULL),
+(20,5,4,0,3,'2020-06-02 18:35:00',NULL);
 
 /*Table structure for table `password_resets` */
 
@@ -157,11 +151,11 @@ CREATE TABLE `product` (
 /*Data for the table `product` */
 
 insert  into `product`(`id`,`name`,`category_id`,`price`,`status`,`created_at`,`updated_at`) values 
-(1,'Samsung S5','Telephone','800',1,'2020-06-02 07:30:56',NULL),
-(2,'Samsung S6 Edge','Telephone','1200',1,'2020-06-02 07:30:56',NULL),
-(3,'Samsung S7','Telephone','1500',1,'2020-06-02 07:30:56',NULL),
-(4,'Samsung S10','Telephone','4500',1,'2020-06-02 07:30:56',NULL),
-(5,'IPhone X','Telephone','7500',1,'2020-06-02 07:30:56',NULL);
+(1,'Samsung S5','Telephone','800',1,'2020-06-02 18:35:00',NULL),
+(2,'Samsung S6 Edge','Telephone','1200',1,'2020-06-02 18:35:00',NULL),
+(3,'Samsung S7','Telephone','1500',1,'2020-06-02 18:35:00',NULL),
+(4,'Samsung S10','Telephone','4500',1,'2020-06-02 18:35:00',NULL),
+(5,'IPhone X','Telephone','7500',1,'2020-06-02 18:35:00',NULL);
 
 /*Table structure for table `users` */
 
@@ -184,12 +178,12 @@ CREATE TABLE `users` (
 /*Data for the table `users` */
 
 insert  into `users`(`id`,`phone`,`email`,`email_verified_at`,`password`,`role`,`remember_token`,`created_at`,`updated_at`) values 
-(1,'992927001911','admin@gmail.com',NULL,'$2y$10$GVmbXkRE4jcZjav3VBjxCeup6GOHGUUe7FQXlkZwguQ0XQ9vl/7Yq','admin',NULL,NULL,NULL),
-(2,'992927001911','muzafarov9797@gmail.com',NULL,'$2y$10$N06.hEMaOf9VCO0gMCa5VuY/F1x.x/U.b3Y9y7NVsaxE9u0R9fQpe','user',NULL,NULL,NULL),
-(3,'992927001911','user1@gmail.com',NULL,'$2y$10$YkBdtWhpU/AsJNfiHo22R.dw5SKXXpPZaSX0IdKd8/1FKrYzuUwlO','user',NULL,NULL,NULL),
-(4,'992927001911','user2@gmail.com',NULL,'$2y$10$Ic1vo7RiuLgmxTwmeSc34e1dM7NvpmmN1.olvsw.pa/NaYIBgdFDa','user',NULL,NULL,NULL),
-(5,'992927001911','user3@gmail.com',NULL,'$2y$10$OLs1AnOHe6OU29QXRL4ZHeNPV.qWRXiFK1IN48.92SxdHrig0LJti','user',NULL,NULL,NULL),
-(6,'992927001911','muzafarov@yandex.ru',NULL,'$2y$10$8RkkBGV/KWp7qakp0rQ0SuTcr2R79.JXBD7v8QI4DboYVNMLbnImO','operator',NULL,NULL,NULL);
+(1,'992917004471','admin@gmail.com',NULL,'$2y$10$XdNZI9T/kNrLfrTEQ1NCVukdtb.rGSOAQzRFHimSM7p2Aks1vc9Ay','admin',NULL,NULL,NULL),
+(2,'992917004471','manu6699@mail.ru',NULL,'$2y$10$2R6m2xMqWDTRb6cXpQmsTO1Vmk5fePLDgXG0H/1XTRfZWUtzREM8.','user',NULL,NULL,NULL),
+(3,'992917004471','manu66992@mail.ru',NULL,'$2y$10$Dxhr8YRtOKu9MyAIUAvfn.rG.vG0oXGPcB6W0Qt6JL8dzQScjEvk6','user',NULL,NULL,NULL),
+(4,'992917004471','manu66991@mail.ru',NULL,'$2y$10$O9RZSnBpihg51SBorUJ3d.vs8xBqZySoZddKQwoHI100Xx9cwXrL6','user',NULL,NULL,NULL),
+(5,'992917004471','user3@gmail.com',NULL,'$2y$10$JA1woFm0A848rZvXSFfFOOnMchhChvD4LccmqlWXspSSEWbkWCEiS','user',NULL,NULL,NULL),
+(6,'992927001911','muzafarov@yandex.ru',NULL,'$2y$10$oKm.arUY1r.Pyp3hGrHaK.CxIbee5W/y.RB.0DZr3LMSLjMLYKAgK','operator',NULL,NULL,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
